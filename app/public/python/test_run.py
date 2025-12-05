46ba9c78ae5bcaf938697ff78468e19c2662406f
李攀(男) <lipanhotface@163.com>
2025-12-05 16:07:20 +0800

    123

--- app/public/python/test_run.py
+++ app/public/python/test_run.py
@@ -4,4 +4,4 @@ from datetime import datetime
 for i in range(1, 51):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"第 {i} 次执行，时间：{now}")
-    time.sleep(2)
+    time.sleep(0.1)
