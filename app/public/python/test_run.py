778fe32f18ef5fb26d003c0556258905ab8831f9
李攀(男) <lipanhotface@163.com>
2025-12-06 00:21:25 +0800

    更新

--- app/public/python/test_run.py
+++ app/public/python/test_run.py
@@ -1,5 +1,11 @@
 import time
 from datetime import datetime
+# -*- coding: utf-8 -*-
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', line_buffering=True)
+
 
 for i in range(1, 51):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
